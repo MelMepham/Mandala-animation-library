8670ec55dc3f4533c491ae67459d8ae4fa3febdd
melmepham <m.a.mepham@gmail.com>
2020-04-12 12:44:38 +1200

    adding icon modules to routing module

--- src/app/mandala-lib/mandala-one/mandala-one-routing.module.ts
+++ src/app/mandala-lib/mandala-one/mandala-one-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MandalaOneComponent } from './mandala-one.component';
+import { StopModule } from '../icons/stop/stop.module';
+import { CancelModule } from '../icons/cancel/cancel.module';
 
 
 
@@ -12,7 +14,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
+    imports: [
+        StopModule,
+        CancelModule,
+        RouterModule.forChild(routes)
+    ],
     exports: [RouterModule]
 })
 export class MandalaOneRoutingModule { }
